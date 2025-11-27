c84b9feeac788583092a61fea9b97dd6bfe1b01c
felipeembersisc <felipe.embersisc@gmail.com>
2025-11-26 23:39:50 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifyCors } from '@fastify/cors'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'API Sistema Maristela',
+      description: 'Sistema para gerenciamento de acampamentos e cursos',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(ScalarApiReference, {
+  routePrefix: '/docs',
+})
+
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log('🚀 HTTP server running on http://localhost:3333')
+  console.log('📚 Docs available at http://localhost:3333/docs')
+})
